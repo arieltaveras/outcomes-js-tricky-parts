a22b775938c55bee3a69196917c0850e86c7a62f
arieltaveras <ariel.taveras7278@gmail.com>
2023-11-07 11:49:46 -0500

    complete

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,19 @@
 function guessingGame() {
+  const answer = Math.floor(Math.random() * 100);
+  let gameOver = false;
+  let guesses = 0;
 
+  return function guess(num) {
+    if (gameOver) return "The game is over, you already won!";
+    guesses++;
+    if (num === answer) {
+      gameOver = true;
+      const guess = guesses === 1 ? "guess" : "guesses";
+      return `You win! You found ${num} in ${guesses} ${guess}.`;
+    }
+    if (num < answer) return `${num} is too low!`;
+    if (num > answer) return `${num} is too high!`;
+  };
 }
 
 module.exports = { guessingGame };
